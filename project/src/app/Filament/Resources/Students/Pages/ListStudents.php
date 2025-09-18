c6736fce92e6080d67292548bde1ee8c0fb9b00d
Anraaa <aqlaharun03@gmail.com>
2025-09-18 22:15:08 +0700

    Added widget for some information

--- project/src/app/Filament/Resources/Students/Pages/ListStudents.php
+++ project/src/app/Filament/Resources/Students/Pages/ListStudents.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Students\Pages;
 use App\Filament\Resources\Students\StudentResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\Students\Widgets\StudentOverview;
 
 class ListStudents extends ListRecords
 {
@@ -14,6 +15,13 @@ class ListStudents extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            StudentOverview::class,
         ];
     }
 }
